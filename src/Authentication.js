5a1d08bf4afc908dddfea9d1c3d26cf8694d4566
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-27 14:30:01 +0100

    chore: create authenticationjs.

---
+++ src/Authentication.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import  firebaseConfig from "./firebase.js";
+import { makeStyles } from '@material-ui/core/styles';
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme) => ({
+   loading: {
+      color: 'white',
+      textAlign: 'center',
+      fontFamily: 'Segoe UI',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontWeight: 900,
+      fontSize: '50px',
+      height: '100vh'
+   }
+}));
+  
+
+export const AuthContext = React.createContext();
+
+
+
+export const AuthProvider = ({ children }) => {
+    const classes = useStyles();
+
+    const [currentUser, setCurrentUser] = useState(null);
+    const [pending, setPending] = useState(true);
+    useEffect(() => {
+      firebaseConfig.auth().onAuthStateChanged((user) => {
+          setCurrentUser(user)
+          setPending(false)
+        });
+    }, []);
+    if(pending){
+        return <Box className={classes.loading} >Please wait...</Box>
+    }
+    return (
+        <AuthContext.Provider
+          value={{
+            currentUser
+          }}
+        >
+          {children}
+        </AuthContext.Provider>
+    );
+};
