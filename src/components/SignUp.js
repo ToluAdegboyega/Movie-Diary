c7001ef35fe75d71af231739cc02ae2b51b76823
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-27 14:31:37 +0100

    feat: create signup page.

---
+++ src/components/SignUp.js
@@ -0,0 +1,126 @@
+import React, { useCallback} from 'react';
+import { Link } from 'react-router-dom'
+import firebaseConfig from "../firebase";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: theme.spacing(8),
+    padding: theme.spacing(8),
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor:  'white',
+    width:  '100%',
+    borderRadius: '5px'
+  },
+  paperH1: {
+    color: 'black',
+    textAlign: 'center',
+    fontFamily: 'Segoe UI'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp({  history  }) {
+  const classes = useStyles();
+
+  const handleSignUp = useCallback(async event => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    try {
+      await firebaseConfig
+        .auth()
+        .createUserWithEmailAndPassword(email.value, password.value);
+      history.push("/Movie-Crud");
+    } catch (error) {
+      alert(error);
+    }
+    }, [history]);
+
+  return (
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5" className={classes.paperH1}>
+          Sign up
+        </Typography>
+        <form onSubmit={handleSignUp} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link to="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+  );
+}
