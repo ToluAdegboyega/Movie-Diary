c7b21259f8879e971dc9ac4c6393258bca7e06c9
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-27 14:29:03 +0100

    feat: add routing.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Movies from './components/Movies';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 import  './assets/App.css';
+import  PrivateRoute  from  './PrivateRoute';
+import  {AuthProvider}  from  './Authentication';
 
 function App() {
   return (
-    <Router>
+    <AuthProvider>
+      <Router>
        <div className='movieWrapper' >
-        <Route exact path="/Movie-Crud" component={Movies} />
+          <PrivateRoute exact path="/Movie-Crud" component={Movies} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
       </div>
     </Router>
+    </AuthProvider>
   );
 }
 
