7b8867d3018f3c43ca115ed72bba1b054f3dc1f6
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-27 12:39:42 +0100

    chore: create usertable.

---
+++ src/components/UserTable.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import  '../assets/UserTable.css';
+
+const UserTable = props => (
+  <table    className="table table-borderless table-stripped">
+    <thead  className="thead-light">
+      <tr>
+        <th>Movie Title</th>
+        <th>Year Released</th>
+        <th> Review</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id} className="table-row">
+            <td>{user.name}</td>
+            <td>{user.date}</td>
+            <td>{user.review}</td>
+            <td>
+              <a
+                onClick={() => {
+                  props.editRow(user)
+                }}
+                className="btn text-primary"
+              >
+                <i className="fas fa-pencil-alt"></i>
+              </a>
+              <a
+                onClick={() => props.deleteUser(user.id)}
+                className="btn text-danger"
+              >
+               <i className="far fa-trash-alt"></i>
+              </a>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr className="table-row">
+          <td colSpan={3}>No movies</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default UserTable
