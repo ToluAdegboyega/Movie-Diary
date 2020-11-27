90f7d33d919c0b5ffd2366ff274152b0295ecfc2
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-27 12:56:00 +0100

    chore: add react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Movies from './components/Movies';
 import  './assets/App.css';
 
 function App() {
   return (
-    <div className='movieWrapper' >
-      <Movies></Movies>
-    </div>
+    <Router>
+       <div className='movieWrapper' >
+        <Route exact path="/Movie-Crud" component={Movies} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+      </div>
+    </Router>
   );
 }
 
